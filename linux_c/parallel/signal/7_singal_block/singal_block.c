a75cd6222fc075916a767e3538285a29472be7ab
gsp1004 <564170877@qq.com>
2021-11-08 17:32:59 +0800

    improve: add some code to save the (signal handler and signal mask) before enter the function, and revert them when quit the funtion

--- linux_c/parallel/signal/7_singal_block/singal_block.c
+++ linux_c/parallel/signal/7_singal_block/singal_block.c
@@ -33,10 +33,44 @@ void unblock_signal(int signal)
 	sigprocmask(SIG_UNBLOCK, &s, NULL);
 }
 
+//save the signal status, when finish the call, revert it
+typedef enum
+{
+	ENV_SAVE,
+	ENV_REVERT,
+}env_op_e;
+
+void save_env(env_op_e env)
+{
+	static sigset_t s;
+	if(ENV_SAVE == env){
+		sigset_t tmp;
+		sigemptyset(&tmp);
+		sigemptyset(&s);
+		sigprocmask(SIG_UNBLOCK, &tmp, &s);  // save env signal to s
+	}
+	else{
+		sigprocmask(SIG_SETMASK, &s, NULL);
+	}
+
+}
+/*
+   typedef void (*sighandler_t)(int);
+
+   sighandler_t signal(int signum, sighandler_t handler);
+
+
+ */
+
 int main()
 {
 	usage();
-	signal(SIGINT, int_handler);
+
+	void (*sighandler_before)(int);
+	sighandler_before = signal(SIGINT, int_handler);
+
+	save_env(ENV_SAVE);
+
 	for(int j=0;j<5;j++){
 		// block signal
 		block_signal(SIGINT);
@@ -51,5 +85,9 @@ int main()
 		write(1, "\n", 1);
 	}
 	putchar('\n');
+
+	save_env(ENV_REVERT);
+	signal(SIGINT, sighandler_before);
+
 	return 0;
 }
